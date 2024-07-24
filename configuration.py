d4516da137ced2d91f5f7d142b37b5106c5c7cc4
HellGranny <mrs.torture@yandex.ru>
2024-07-24 14:17:17 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://d2d0d31f-f08c-4a09-9b76-f57a1c25afa9.serverhub.praktikum-services.ru"
+CREAT_ORDERS = "/api/v1/orders"
+TRACK_ORDER = "/api/v1/orders/track"
---
+++ sender_stand_request.py
@@ -0,0 +1,19 @@
+# Полякова Елизавета, 19 когорта Venus - Финальный проект. Инженер по тестированию плюс
+import configuration
+import requests
+import data
+
+
+# Создание заказа
+def create_order(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREAT_ORDERS,
+                         json=body)
+
+
+# Заказ по номеру трекера
+def get_order(track_number):
+    get_order_url = f"{configuration.URL_SERVICE}{configuration.TRACK_ORDER}?t={track_number}"
+    response = requests.get(get_order_url)
+    return response
+
+
